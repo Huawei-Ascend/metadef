a4f4ea3ccbac9c964dfd7c9820e942b5e02eff72
jelly.lj <jelly.lijia@huawei.com>
2020-11-30 16:40:03 +0800

    [optiling]Delete the unused interface and refresh the code format.

--- register/op_tiling_registry.cpp
+++ register/op_tiling_registry.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2019-2020 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,46 +21,26 @@
 
 namespace optiling {
 
-constexpr int UUID_LENGTH = 32;
-
-std::string GenUuid()
-{
-    static std::random_device dev;
-    static std::mt19937 rng(dev());
-    const char *v = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    std::uniform_int_distribution<int> dist(0, std::strlen(v) - 1);
-    char res[UUID_LENGTH + 1] = {0};
-    for (size_t i = 0; i < sizeof(res) - 1; i++) {
-        res[i] = v[dist(rng)];
-    }
-    return res;
+std::map<std::string, OpTilingFunc> &OpTilingRegistryInterf::RegisteredOpInterf() {
+  static std::map<std::string, OpTilingFunc> interf;
+  return interf;
 }
 
-std::string OpTilingInterf::OpTilingUuid = GenUuid();
-
-std::map<std::string, OpTilingFunc> & OpTilingInterf::RegisteredOpInterf() {
-    static std::map<std::string, OpTilingFunc> interf;
-    return interf;
+OpTilingRegistryInterf::OpTilingRegistryInterf(std::string op_type, OpTilingFunc func) {
+  auto &interf = RegisteredOpInterf();
+  interf.emplace(op_type, func);
+  GELOGI("Register tiling function: op_type:%s, funcPointer:%p, registered count:%zu", op_type.c_str(),
+         func.target<OpTilingFuncPtr>(), interf.size());
 }
 
+size_t ByteBufferGetAll(ByteBuffer &buf, char *dest, size_t dest_len) {
+  size_t nread = 0;
+  size_t rn = 0;
+  do {
+    rn = buf.readsome(dest + nread, dest_len - nread);
+    nread += rn;
+  } while (rn > 0 && dest_len > nread);
 
-OpTilingInterf::OpTilingInterf(std::string op_type, OpTilingFunc func) {
-    auto &interf = RegisteredOpInterf();
-    interf.emplace(op_type, func);
-    GELOGI("Register tiling function: op_type:%s, funcPointer:%p, registered count:%zu",
-           op_type.c_str(), func.target<OpTilingFuncPtr>(), interf.size());
+  return nread;
 }
-
-std::map<std::string, OpTilingFuncNew> & OpTilingRegistryInterf::RegisteredOpInterf() {
-    static std::map<std::string, OpTilingFuncNew> interf_new;
-    return interf_new;
-}
-
-OpTilingRegistryInterf::OpTilingRegistryInterf(std::string op_type, OpTilingFuncNew func) {
-    auto &interf_new = RegisteredOpInterf();
-    interf_new.emplace(op_type, func);
-    GELOGI("Register tiling function: op_type:%s, funcPointer:%p, registered count:%zu",
-           op_type.c_str(), func.target<OpTilingFuncPtrNew>(), interf_new.size());
-}
-}
+}  // namespace optiling
